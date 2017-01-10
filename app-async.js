40bedd1fc7f0682de8aa9299ad0c2499fc1b52ce
zwjay <474338731@qq.com>
2017-01-10 22:42:43 +0800

    First
    Signed-off-by: zwjay <474338731@qq.com>

--- app-async.js
+++ app-async.js
@@ -1,21 +1,45 @@
-var async = require('async');
-var concurrencyCount = 0;
-var fetchUrl = function (url, callback) {
-	var delay = parseInt((Math.random() * 10000000) % 2000, 10);
-	concurrencyCount++;
-	console.log('现在的并发数是', concurrencyCount, '，正在抓取的是', url, '，耗时' + delay + '毫秒');
-	setTimeout(function () {
-		concurrencyCount--;
-		callback(null, url + ' html content');
-	}, delay);
-};
-var urls = [];
-for (var i = 0; i < 30; i++) {
-	urls.push('http://datasource_' + i);
+// var async = require('async');
+// var concurrencyCount = 0;
+// var fetchUrl = function (url, callback) {
+// 	var delay = parseInt((Math.random() * 10000000) % 2000, 10);
+// 	concurrencyCount++;
+// 	console.log('现在的并发数是', concurrencyCount, '，正在抓取的是', url, '，耗时' + delay + '毫秒');
+// 	setTimeout(function () {
+// 		concurrencyCount--;
+// 		callback(null, url + ' html content');
+// 	}, delay);
+// };
+// var urls = [];
+// for (var i = 0; i < 30; i++) {
+// 	urls.push('http://datasource_' + i);
+// }
+// async.mapLimit(urls, 5, function (url, callback) {
+// 	fetchUrl(url, callback);
+// }, function (err, result) {
+// 	console.log('final:');
+// 	console.log(result);
+// });
+var ddd = [['http://imglf0.nosdn.127.net/img/anA5K0szZ3JKNFB1YkZraExQNGdDRWJPajFleUYvZk9HaDNISVErb0JiSHJpMkJtU3RKc1lRPT0.jpg_anA5K0szZ3JKNFB1YkZraExQNGdDRWJPajFleUYvZk9HaDNISVErb0JiSHJpMkJtU3RKc1lRPT0.jpg',
+	'http://imglf0.nosdn.127.net/img/anA5K0szZ3JKNFBuMkcyRlRBdGh1SVZxMVI5QkJONHNCUDhEMG9KNytFZ0tXV0RLY2FCaEl3PT0.jpg_anA5K0szZ3JKNFBuMkcyRlRBdGh1SVZxMVI5QkJONHNCUDhEMG9KNytFZ0tXV0RLY2FCaEl3PT0.jpg',
+	'http://imglf2.nosdn.127.net/img/anA5K0szZ3JKNE5kZGxXU2xGYjNCSUR4TVl2QnRCeGlRRE1ncTllSDVFUVp1WFZIbGpqNS9RPT0.jpg_anA5K0szZ3JKNE5kZGxXU2xGYjNCSUR4TVl2QnRCeGlRRE1ncTllSDVFUVp1WFZIbGpqNS9RPT0.jpg',
+	'http://imglf2.nosdn.127.net/img/anA5K0szZ3JKNE5iWDR4UkVuTVMyMENUMG10WUpESUs5ZUFVV09ZMElMRFRRWHpkSlZnR25BPT0.jpg_anA5K0szZ3JKNE5iWDR4UkVuTVMyMENUMG10WUpESUs5ZUFVV09ZMElMRFRRWHpkSlZnR25BPT0.jpg',
+	'http://imglf0.nosdn.127.net/img/anA5K0szZ3JKNE5iWDR4UkVuTVMyOFhFdWduc1JTRVZsV2w0dmU4WC9LekM3RjRLZVF0THZnPT0.jpg_anA5K0szZ3JKNE5iWDR4UkVuTVMyOFhFdWduc1JTRVZsV2w0dmU4WC9LekM3RjRLZVF0THZnPT0.jpg',
+	'http://imglf1.nosdn.127.net/img/U0JJdGZZRW55T3R1UVhsY2V2NEJoaWJzZi9MZzhlZ2dKSzF5eTY1anR4bmt4b0xib2JiZ3ZRPT0.jpg_U0JJdGZZRW55T3R1UVhsY2V2NEJoaWJzZi9MZzhlZ2dKSzF5eTY1anR4bmt4b0xib2JiZ3ZRPT0.jpg',
+	'http://imglf.nosdn.127.net/img/U0JJdGZZRW55T3R1UVhsY2V2NEJodSt4R3BtWGc3STVNUFJ6QzNQZ083MG5rZ1E1c0NpV2ZnPT0.jpg_U0JJdGZZRW55T3R1UVhsY2V2NEJodSt4R3BtWGc3STVNUFJ6QzNQZ083MG5rZ1E1c0NpV2ZnPT0.jpg',
+	'http://imglf.nosdn.127.net/img/U0JJdGZZRW55T3R1UVhsY2V2NEJodjQ3ak0xVXlXSnVwaDArTzl4ZEpzZHZ0alZ4MzdYK1ZBPT0.jpg_U0JJdGZZRW55T3R1UVhsY2V2NEJodjQ3ak0xVXlXSnVwaDArTzl4ZEpzZHZ0alZ4MzdYK1ZBPT0.jpg',
+	'http://imglf0.nosdn.127.net/img/U0JJdGZZRW55T3R1UVhsY2V2NEJoZ0xZSk53UnBDKzBNbVdiSm1XQWtobVAxZldTdXlQZ2ZRPT0.jpg_U0JJdGZZRW55T3R1UVhsY2V2NEJoZ0xZSk53UnBDKzBNbVdiSm1XQWtobVAxZldTdXlQZ2ZRPT0.jpg',
+	'http://imglf.nosdn.127.net/img/dGJ1Z2Z3ZFFJdnIzeTBOOWhjOUJORDJpKzFCQk1XTEVpUTlTU2kzRGhCckhWTHl3RUxFSjJRPT0.jpg_dGJ1Z2Z3ZFFJdnIzeTBOOWhjOUJORDJpKzFCQk1XTEVpUTlTU2kzRGhCckhWTHl3RUxFSjJRPT0.jpg'],
+	['http://imglf0.nosdn.127.net/img/WHpCTzN4M0dsMzZPbzQvcFRETnBLNndUb3YxcnduMTVuakZabDdJc3FOemExamFYY2VxTG9BPT0.jpg_WHpCTzN4M0dsMzZPbzQvcFRETnBLNndUb3YxcnduMTVuakZabDdJc3FOemExamFYY2VxTG9BPT0.jpg',
+		'http://imglf1.nosdn.127.net/img/eExtQjZkbVJDTHRJMnFnSktUMllvenltWjhXR1ljZUZqSzk0amhrQWRHS3BvbWtrbHdOanNRPT0.jpg_eExtQjZkbVJDTHRJMnFnSktUMllvenltWjhXR1ljZUZqSzk0amhrQWRHS3BvbWtrbHdOanNRPT0.jpg',
+		'http://imglf2.nosdn.127.net/img/eExtQjZkbVJDTHZSbjc0L2hBbDIxeHptUEcza2ZmTkFGVlRodThFaENXa2F5dEFCQXg5dmtBPT0.jpg_eExtQjZkbVJDTHZSbjc0L2hBbDIxeHptUEcza2ZmTkFGVlRodThFaENXa2F5dEFCQXg5dmtBPT0.jpg',
+		'http://imglf0.nosdn.127.net/img/eExtQjZkbVJDTHRiSEUxejJLSEVaR25mRmFBRzQxUk1Ra0xQTTdjLytLeTgzRys2MG1qTkF3PT0.jpg_eExtQjZkbVJDTHRiSEUxejJLSEVaR25mRmFBRzQxUk1Ra0xQTTdjLytLeTgzRys2MG1qTkF3PT0.jpg',
+		'http://imglf0.nosdn.127.net/img/anA5K0szZ3JKNE5PNHpUYWNjdnFLakNZRnBpRTcwV1haeEJibFYxbEM5T0RKeEJpZTJrNmtnPT0.jpg_anA5K0szZ3JKNE5PNHpUYWNjdnFLakNZRnBpRTcwV1haeEJibFYxbEM5T0RKeEJpZTJrNmtnPT0.jpg',
+		'http://imglf0.nosdn.127.net/img/U0JJdGZZRW55T3RoRzlQUWIzRXVqUDcyWWNzb1Z5UGtYdzlXQlFQUmdXWVY4MUdHb1dBMk1BPT0.jpg_U0JJdGZZRW55T3RoRzlQUWIzRXVqUDcyWWNzb1Z5UGtYdzlXQlFQUmdXWVY4MUdHb1dBMk1BPT0.jpg',
+		'http://imglf1.nosdn.127.net/img/U0JJdGZZRW55T3NVY0pnUldmTjFkeTc1dkJaV3V3bXNRQnBGRnpudE1VN3Q3TGFjRzljZGZnPT0.jpg_U0JJdGZZRW55T3NVY0pnUldmTjFkeTc1dkJaV3V3bXNRQnBGRnpudE1VN3Q3TGFjRzljZGZnPT0.jpg',
+		'http://imglf2.nosdn.127.net/img/U0JJdGZZRW55T3NVY0pnUldmTjFkMDdCVTFyblZOdHRYdWVtbjQyUXlOaHJaVExYQ011Q0JnPT0.jpg_U0JJdGZZRW55T3NVY0pnUldmTjFkMDdCVTFyblZOdHRYdWVtbjQyUXlOaHJaVExYQ011Q0JnPT0.jpg',
+		'http://imglf0.nosdn.127.net/img/U0JJdGZZRW55T3NVY0pnUldmTjFkNDJ2dFo0VW1lYmVTNTVXK01YS1BuTkNHdGp0R3JURlhnPT0.jpg_U0JJdGZZRW55T3NVY0pnUldmTjFkNDJ2dFo0VW1lYmVTNTVXK01YS1BuTkNHdGp0R3JURlhnPT0.jpg',
+		'http://imglf1.nosdn.127.net/img/a282TGQ2eDRlVFdLb3oyOGtYNmVpNngzZUNETXRvVk9CL1REbXNUSWZhb21rb2FzR2ZmVi93PT0.jpg_a282TGQ2eDRlVFdLb3oyOGtYNmVpNngzZUNETXRvVk9CL1REbXNUSWZhb21rb2FzR2ZmVi93PT0.jpg']]
+
+for (var i = 0; i <= ddd.length; i++) {
+	console.log(ddd[i])
 }
-async.mapLimit(urls, 5, function (url, callback) {
-	fetchUrl(url, callback);
-}, function (err, result) {
-	console.log('final:');
-	console.log(result);
-});
--- test2.js
+++ test2.js
@@ -19,50 +19,34 @@ var url = 'http://idheihei.lofter.com/tag/%E6%80%A7%E6%84%9F?page=';
 //本地存储目录
 var dir = './lofter';
 // //创建目录
-mkdirp(dir, function(err) {
-	if(err){
+mkdirp(dir, function (err) {
+	if (err) {
 		console.log(err);
 	}
 });
 //抓取頁數
-var page = 6;
-
+var page = 10;
 var urls = [];
-for (var i = 1; i < page; i++) {
+for (var i = 1; i <= page; i++) {
 	urls.push(url + i);
 }
-console.log(urls)
-
-async.mapLimit(urls, 2, function (url, callback) {
-	console.log(url)
-	getImg(url, callback);
-}, function (err, result) {
-	log('1.5 err: ', err);
-	log('1.5 results: ', result);
-});
+console.log('爬取的链接有' + urls);
 var concurrencyCount = 0;
-
-function getImg(urll, callback) {
-	request(urll, function (error, response, body) {
+async.mapLimit(urls, 2, function (url, callback) {
+	request(url, function (error, response, body) {
 		if (!error && response.statusCode == 200) {
+			var imgSrc = [];
+			var delay = Number;
 			var $ = cheerio.load(body);
-			$('.img img').each(function () {
+			$('.img img').each(function (index, value) {
+				delay = parseInt((Math.random() * 10000000) % 2000, 10);
+				concurrencyCount++;
 				var src = $(this).attr('src');
-				// console.log('正在下载' + src);
 				var fileName = src.split("/")[4].split('?')[0];
 				var dowUrl = src.split('?')[0];
-				// console.log(dowUrl + '#' +fileName);
-
-
-				var delay = parseInt((Math.random() * 10000000) % 2000, 10);
-				concurrencyCount++;
-				console.log('现在的并发数是', concurrencyCount, '，正在抓取的是', urll, '，耗时' + delay + '毫秒');
-				setTimeout(function () {
-					concurrencyCount--;
-					callback(null, dowUrl + '#' +fileName);
-				}, delay);
-
-
+				console.log('现在的并发数是', concurrencyCount, '，正在抓取的是', dowUrl, '，耗时' + delay + '毫秒');
+				console.log('值', dowUrl + '_' + fileName);
+				imgSrc.push(dowUrl);
 				// if(page<=1){
 				// 	download(src.split('?')[0], dir, narr).then(function () {
 				// 		// console.log('下载完成');
@@ -72,21 +56,55 @@ function getImg(urll, callback) {
 				// 	});
 				// }
 			});
+			setTimeout(function () {
+				concurrencyCount--;
+				callback(null, imgSrc);
+			}, delay);
 		}
 	});
-}
-
-
+}, function (err, result) {
+	if (err) {
+		console.log('1.5 err: ', err);
+	}
+	console.log('抓取图片完成===================================================================');
+	console.log('1.5 results: ', result);
+	//下载文件;
+	download(result);
+});
 //下载方法
-var download = function (url, dir, filename) {
-	var promise = new Promise(function (resolve, reject) {
-		request.head(url, function (err, res, body) {
-			request(url).on('error', function (err) {
-				console.log(err);
-				reject();
-			}).pipe(fs.createWriteStream(dir +'/'+ filename));
-			resolve();
+var dowconcurrencyCount = 0;
+var download = function (theImgUrl) {
+	for (var i = 0; i < theImgUrl.length; i++) {
+		async.mapLimit(theImgUrl[i], 1, function (url, callback) {
+			// var promise = new Promise(function (resolve, reject) {
+			var delay = Number;
+			request.head(url, function (err, res, body) {
+				delay = parseInt((Math.random() * 10000000) % 2000, 10);
+				dowconcurrencyCount++;
+				request(url).on('error', function (err) {
+					console.log(err);
+					// reject();
+				}).pipe(fs.createWriteStream(dir + '/' + url.split('/')[4]));
+				console.log('现在正在下载', url.split('/')[4], '，耗时' + delay + '毫秒');
+				// resolve();
+				setTimeout(function () {
+					dowconcurrencyCount--;
+					callback(null, url.split('/')[4]);
+				}, delay);
+			});
+			// });
+			// return promise;
+		}, function (err, result) {
+			if (err) {
+				console.log('1.5 err: ', err);
+			}
+			console.log('抓取图片完成===================================================================');
+			console.log('共抓取图片'+result.length+'张');
+			// console.log('1.5 results: ', result);
 		});
-	});
-	return promise;
+	}
 };
+
+
+
+
